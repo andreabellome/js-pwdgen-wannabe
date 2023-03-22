34e24b515ee1b333cc4ed99944440d6d54950672
Andrea Bellome <andrea.bellome@gmail.com>
2023-03-22 17:22:08 +0100

    end excercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 
+const name = prompt( 'Your name:', 'Andrea');
+const surname = prompt( 'Your surname', 'Bellome' );
+const color = prompt( 'Favourite color:', 'Nero' );
+const number = 21.0;
+
+document.getElementById('password').innerHTML = `${ name + surname + color + number }`;
+
+
+
